8bbf1fd2e5dc076b85792fa7c95c0c003c3b1020
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-01 15:46:35 +0530

    exception-chaining

---
+++ exception/ExceptionChaining.java
@@ -0,0 +1,24 @@
+package exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+class MyException extends Exception{
+    MyException(){
+        System.out.println("File Not Found");
+    }
+}
+public class ExceptionChaining {
+    public static void main(String[] args) throws MyException {
+        try{
+            BufferedReader reader=new BufferedReader(new FileReader("demo.txt"));
+            String line;
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+            reader.close();
+        }catch (IOException e){ 
+            throw new  MyException();
+        }
+    }
+}
